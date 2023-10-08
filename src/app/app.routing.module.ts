738042cfdf952554b2d3ede264d903512f68e5ac
Thiago_Nunes <thiago833@hotmail.com>
2023-10-08 09:34:48 -0300

    feat: Redirect route para iniciar na tela adicionar

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'gerenciamento-tarefas', pathMatch: 'full' },
+    { path: '', redirectTo: '/gerenciamento-tarefas/adicionar', pathMatch: 'full' },
+    { path: 'gerenciamento-tarefas', redirectTo: '/gerenciamento-tarefas/adicionar', pathMatch: 'full' },
     {
         path: 'gerenciamento-tarefas',
         loadChildren: async () => (await import('./features/gerenciamento-tarefas/module/gerenciamento-tarefas.module')).GerenciamentoTarefasModule
